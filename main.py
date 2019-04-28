fb94a1d3641829c11f91c8ce678fecae5c1bc532
Peter White <pwhitedeveloper@gmail.com>
2019-04-28 11:33:36 -0400

    Play again added

--- main.py
+++ main.py
@@ -3,82 +3,109 @@ from Card import *
 from Deck import *
 
 playingDeck = Deck()
-player = Hand()
-
-playingDeck.shuffleCards()
 
 def score(player, dealer):
     print("\n")
     print("===========================")
     print(f"Player: {player.getHand()}")
     print(f"Dealer: {dealer.getHand()}")
+    print("")
+    print(f"{playingDeck.remainingCards()} cards in Deck")
     print("===========================")
     print("\n")
 
 player = Hand()
 dealer = Hand()
+playingDeck.shuffleCards()
 
-print("Player starts with a {} and a {}".format(player.drawCard(playingDeck.dealCard()).getTitle(),player.drawCard(playingDeck.dealCard()).getTitle()))
-print("Dealer starts with a {}".format(dealer.drawCard(playingDeck.dealCard()).getTitle()))
+def playGame():
 
-while True:
+    if playingDeck.remainingCards() < 20:
+        print("Reshuffling Decks")
+        playingDeck.shuffleCards()
 
-    if player.getHand() > 21:
-        print("Player's hold is {}".format(player.getHand()))
-        print("Player Busts")
-        player.setBust()
-        break
-    if player.getHand() == 21:
-        print("Player's hold is {}".format(player.getHand()))
-        print("Player Blackjack's")
-        player.setBlackjack()
-        break
+    print("\n")
+    print("Player starts with a {} and a {}".format(player.drawCard(playingDeck.dealCard()).getTitle(),player.drawCard(playingDeck.dealCard()).getTitle()))
+    print("Dealer starts with a {}".format(dealer.drawCard(playingDeck.dealCard()).getTitle()))
+    print("\n")
 
-    score(player, dealer)
+    while True:
 
-    move = input("Do you want to hit or stand? ").lower()
-
-    if move == "hit":
-        print(f"\nYou draw a {player.drawCard(playingDeck.dealCard()).getTitle()}")
-    elif move == "stand":
-        print("\nPlayer Stand's")
-        break
-    else:
-        print("\nInvalid")
-
-print("\nDealer flips a {}".format(dealer.drawCard(playingDeck.dealCard()).getTitle()))
-
-while True:
-    score(player, dealer)
-
-    if not player.getBust():
-        if dealer.getHand() > 21:
-            print("Dealer busts")
-            dealer.setBust()
+        if player.getHand() > 21:
+            print("Player's hold is {}".format(player.getHand()))
+            print("Player Busts")
+            player.setBust()
             break
-        elif dealer.getHand() == 21:
-            print("Dealer Blackjack's")
-            dealer.setBlackjack()
+        if player.getHand() == 21:
+            print("Player's hold is {}".format(player.getHand()))
+            print("Player Blackjack's")
+            player.setBlackjack()
             break
-        elif dealer.getHand() >= 17:
-            print("Dealer stands")
+
+        score(player, dealer)
+
+        move = input("Do you want to hit or stand? ").lower()
+
+        if move == "hit":
+            print(f"\nYou draw a {player.drawCard(playingDeck.dealCard()).getTitle()}")
+        elif move == "stand":
+            print("\nPlayer Stand's")
             break
         else:
-            print("\nDealer draws a {}".format(dealer.drawCard(playingDeck.dealCard()).getTitle()))
-    else:
-        break
+            print("\nInvalid")
 
-if player.getHand() == dealer.getHand():
-    print("\nPush")
-elif player.getBust() == True:
-    print("\nDealer Wins By Default")
-elif dealer.getBust() == True:
-    print("\nPlayer Wins By Default")
-elif player.getBlackjack() == True:
-    print("\nPlayer Wins By Blackjack")
-elif player.getHand() > dealer.getHand():
-    print("\nPlayer Wins")
-elif dealer.getBlackjack() == True:
-    print("\nDealer Wins By Blackjack")
-else:
-    print("\nDealer Wins")
+    print("\nDealer flips a {}".format(dealer.drawCard(playingDeck.dealCard()).getTitle()))
+
+    while True:
+        score(player, dealer)
+
+        if not player.getBust():
+            if dealer.getHand() > 21:
+                print("Dealer busts")
+                dealer.setBust()
+                break
+            elif dealer.getHand() == 21:
+                print("Dealer Blackjack's")
+                dealer.setBlackjack()
+                break
+            elif dealer.getHand() >= 17:
+                print("Dealer stands")
+                break
+            else:
+                print("\nDealer draws a {}".format(dealer.drawCard(playingDeck.dealCard()).getTitle()))
+        else:
+            break
+
+    if player.getHand() == dealer.getHand():
+        print("\nPush")
+    elif player.getBust() == True:
+        print("\nDealer Wins By Default")
+    elif dealer.getBust() == True:
+        print("\nPlayer Wins By Default")
+    elif player.getBlackjack() == True:
+        print("\nPlayer Wins By Blackjack")
+    elif player.getHand() > dealer.getHand():
+        print("\nPlayer Wins")
+    elif dealer.getBlackjack() == True:
+        print("\nDealer Wins By Blackjack")
+    else:
+        print("\nDealer Wins")
+
+game_over = False
+while not game_over:
+    playGame()
+
+    while True:
+        ans = input("\nPlay another hand (y/n)? ").lower()
+
+        if ans == "n":
+            print("Come back soon with more money")
+            game_over = True
+            break
+        elif ans == "y":
+            print("Dealing again")
+            player.resetHand()
+            dealer.resetHand()
+            break
+        else:
+            print("Invalid")
